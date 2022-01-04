0772b99f46c35378fcbd9ce5cfa52e2b4fe644ba
Vova <70287671+neshchadym@users.noreply.github.com>
2022-01-04 17:09:30 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(n) {
+    var num =
+        "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(
+            " "
+        );
+    var tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(
+        " "
+    );
+
+
+    if (n < 20) return num[n];
+    var digit = n % 10;
+    if (n < 100)
+        return tens[~~(n / 10) - 2] + (digit ? "-" + num[digit] : "");
+    if (n < 1000)
+        return (
+            num[~~(n / 100)] +
+            " hundred" +
+            (n % 100 == 0 ? "" : " " + toReadable(n % 100))
+        );
+    return (
+        toReadable(~~(n / 1000)) +
+        " thousand" +
+        (n % 1000 != 0 ? " " + toReadable(n % 1000) : "")
+    );
+};
